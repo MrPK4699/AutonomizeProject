41f53cc4acbb95240dfaf551d551f018bff2ebe7
Pankaj Khatri <4699pankajkhatri@gmail.com>
2025-01-09 21:10:41 +0530

    nodemon installed and userSchema created

---
+++ backend/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  name: String,
+  bio: String,
+  blog: String,
+  location: String,
+  public_repos: Number,
+  public_gists: Number,
+  followers: Number,
+  following: Number,
+  avatar_url: String,
+  isDeleted: { type: Boolean, default: false },
+  created_at: Date,
+  updated_at: Date,
+  friends: [{ type: String }],
+});
+
+module.exports = mongoose.model('User', userSchema);
